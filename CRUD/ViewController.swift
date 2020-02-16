5c32302e8c620532a61491be074ebf4027e883a4
wanhar <wanhardaengmaro@gmail.com>
2020-02-16 23:35:30 +0700

    search bar

--- CRUD/ViewController.swift
+++ CRUD/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
 
+     @IBOutlet weak var tableViewUser: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initView()
+    }
+    
+    func initView(){
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        searchController.searchBar.placeholder  = "Cari User"
+        
+        self.tableViewUser.tableHeaderView   = searchController.searchBar
+        self.navigationController?.navigationBar.barTintColor   = UIColor(displayP3Red: 0.14, green: 0.86, blue: 0.73, alpha: 1.0)
+    //    self.navigationController?.navigationBar.tintColor = .red
+        self.navigationController?.navigationBar.titleTextAttributes    = [NSAttributedString.Key.foregroundColor: UIColor.green]
+        self.navigationController?.navigationBar.largeTitleTextAttributes   = [NSAttributedString.Key.foregroundColor: UIColor.green]
+        
     }
 
 
