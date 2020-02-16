c76fbcbe6c1994483f4723dcae0fbf6bae52f515
wanhar <wanhardaengmaro@gmail.com>
2020-02-16 23:12:12 +0700

    Form insert user

---
+++ CRUD/AddUserViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AddUserViewController.swift
+//  CRUD
+//
+//  Created by Wanhar on 16/02/20.
+//  Copyright © 2020 Wanhar. All rights reserved.
+//
+
+import UIKit
+
+class AddUserViewController: UIViewController {
+    
+    @IBOutlet weak var textName: UITextField!
+    @IBOutlet weak var textEmail: UITextField!
+    @IBOutlet weak var textGender: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CRUD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CRUD
+//
+//  Created by Wanhar on 16/02/20.
+//  Copyright © 2020 Wanhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
